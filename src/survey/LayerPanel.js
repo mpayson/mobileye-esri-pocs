0c7be67163d03c16a62d102c89087bbae5a829ca
Or Rappel-Kroyzer <orr@mobileye.com>
2019-09-24 15:32:08 -0700

    Addded missing files

---
+++ src/survey/LayerPanel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { observer } from "mobx-react";
+import { Card } from 'antd';
+import HistMinMaxSlideFilter from '../components/HistMinMaxSlideFilter';
+
+const LayerPanel = observer(class LayerPanel extends React.Component{
+
+  render(){
+
+    const FilterViews = this.props.store.filters.map(f => {
+      switch(f.type){
+        case 'minmax':
+          return <HistMinMaxSlideFilter store={f} key={f.field}/>
+        default:
+          throw new Error("Unknown filter type!");
+      }
+    })
+
+    return (
+      <>
+        <Card size="small">
+          <h1>Explore</h1>
+        </Card>
+        <Card size="small" style={{marginTop: "10px"}}>
+          <h1>Apply Filters</h1>
+          {FilterViews}
+        </Card>
+      </>
+    )
+  }
+
+});
+
+export default LayerPanel;
