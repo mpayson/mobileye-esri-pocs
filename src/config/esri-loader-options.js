866575432ae309fd4935b66423eedb1ae834de73
mpayson <mpayson@esri.com>
2019-10-28 07:19:12 -0400

    bump back up to 4.13

--- src/config/esri-loader-options.js
+++ src/config/esri-loader-options.js
@@ -1,9 +1,9 @@
 const options = {
-  url: "https://js.arcgis.com/4.12/",
+  url: "https://js.arcgis.com/4.13/",
   // url: "https://js.arcgis.com/4.12/",
   // css: "/jscss/my-theme/main.css",
   // css: true,
-  css: "https://js.arcgis.com/4.12/esri/themes/light/main.css",
+  css: "https://js.arcgis.com/4.13/esri/themes/light/main.css",
   dojoConfig: {
     has: {
       "esri-native-promise": true
