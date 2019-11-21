2cffa0fba14f1a25fe91061872839eb152e9f04f
ikaravan <ido.karavany@intel.com>
2019-11-21 13:29:24 +0200

    add set mapid

--- esri_tools/esri_tools.py
+++ esri_tools/esri_tools.py
@@ -8,7 +8,7 @@ def get_webmap_id(map_postfix):
     username = 'dynrem.generic'
     password = 'Dynr3mG3neric'
     gis = GIS(username=username, password=password)
-    webmap_name = f"dynrem-{environment}-{map_postfix}"
+    webmap_name = "dynrem-" + environment + "-" + map_postfix
     webmap_list = gis.content.search(webmap_name, item_type="Web Map")
     webmap_id = webmap_list[0].id
     return webmap_id
