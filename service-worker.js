282a39bade4011ef2c5528eab6e7c633d53d1fa0
Or Rappel-Kroyzer <orr@mobileye.com>
2019-12-08 13:47:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mobileye-esri-pocs/precache-manifest.28061ac6bccffc85f03879773c574437.js"
+  "/mobileye-esri-pocs/precache-manifest.38de40ecd4775f9e695e4f727e82e90c.js"
 );
 
 self.addEventListener('message', (event) => {
