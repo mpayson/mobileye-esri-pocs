b5db839d107f429100fd64161cd921af6b650d37
dvorovyx <dmitryx.vorovyev@intel.com>
2020-01-29 13:05:22 +0300

    events: upd icons

--- src/events/EventsApp.js
+++ src/events/EventsApp.js
@@ -90,7 +90,6 @@ const EventsApp = observer(class App extends React.Component {
           style: 'card',
           layout: 'stack',
         });
-        console.log(this.store.legendLayerInfos);
         this.view.ui.add(searchExpand, "top-right");
         this.view.ui.add(legend, "bottom-right");
         this.view.ui.move("zoom", "top-right");
