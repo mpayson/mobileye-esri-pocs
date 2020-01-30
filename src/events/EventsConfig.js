e02a55fd660a1582238e32c12cb6d459a85d1aef
dvorovyx <dmitryx.vorovyev@intel.com>
2020-01-30 17:40:30 +0300

    events: basic custom tooltip (speed / time)

--- src/events/EventsConfig.js
+++ src/events/EventsConfig.js
@@ -195,7 +195,9 @@ const eventsConfig = {
   ],
   hasCustomTooltip: true,
   statisticsFieldsInfo: { 
-    'avg_last_hour': {title: 'Average speed', postText: 'km/h', iconTag: 'speed'},
+    'avg_last_15_min':  {title: 'Average speed for the last 15 minutes',  postText: 'km/h', iconTag: 'speed'},
+    'avg_last_hour':    {title: 'Average speed for the last hour',        postText: 'km/h', iconTag: 'speed'},
+    'avg_last_3_hours': {title: 'Average speed for the last 3 hours',     postText: 'km/h', iconTag: 'speed'},
     // 'insert_ts': {title: 'Detection time'},
   },
   timestampFieldsInfo: {
--- src/events/EventsTooltip.js
+++ src/events/EventsTooltip.js
@@ -48,7 +48,7 @@ const EventsTooltip = observer(({store}) => {
     results[prefix] = {count: 0, sum: 0}
   }
 
-  for(const graphic of graphics){
+  for (const graphic of graphics) {
       const attrs = graphic.attributes;
       for (const prefix of Object.keys(results)) {
           const value = attrs[prefix]
@@ -58,6 +58,10 @@ const EventsTooltip = observer(({store}) => {
           }
       }
   }
+
+  const activeLayer = graphics.slice(-1)[0].layer;
+  const activeTimeFilter = activeLayer.renderer.field;
+
   const colStyle = {
     lineHeight: 1.1, 
     marginTop: '12px',
@@ -75,37 +79,30 @@ const EventsTooltip = observer(({store}) => {
     height: 28, 
     fill: '#6e6e6e'
   };
-  const infoContent = Object.entries(eventConfig.statisticsFieldsInfo).map(entry => { 
-    const [key, params] = entry;
-    const IconSvg = TAG_TO_SVG[params.iconTag] || NoIcon;
-    const value = results[key].count ? Math.round(results[key].sum / results[key].count) : 0;
-    return value ? (
-        <Col key={key} span={12} style={colStyle}>
-          <IconSvg {...iconProps} title={params.title}/>
-          <span>
-            <span style={{fontSize: '24px'}}>
-              {value}  
+
+  const infoContent = Object.entries(eventConfig.statisticsFieldsInfo)
+    .filter(([key, _]) => key === activeTimeFilter)
+    .map(([key, params]) => { 
+      const IconSvg = TAG_TO_SVG[params.iconTag] || NoIcon;
+      const value = results[key].count ? Math.round(results[key].sum / results[key].count) : 0;
+      return value ? (
+          <Col key={key} span={12} style={colStyle}>
+            <IconSvg {...iconProps} title={params.title}/>
+            <span>
+              <span style={{fontSize: '24px'}}>
+                {value}  
+              </span>
+              {' '}{params.postText}
             </span>
-            {' '}{params.postText}
-          </span>
-        </Col>
-    ) : null;
-  })
+          </Col>
+      ) : null;
+    })
 
   const timeContent = Object.entries(eventConfig.timestampFieldsInfo).map(entry => { 
     const [key, params] = entry;
     const last = graphics.slice(-1)[0]['attributes'];
     const value = last[key];
-    let IconSvg = TAG_TO_SVG[params.iconTag];
-    // if (last['eventType']) {
-    //   const eventInfo = store.renderers['eventType'].uniqueValueInfos
-    //     .find(evt => evt.value === last['eventType']);
-    //   if (eventInfo && eventInfo.symbol.url) {
-    //     const alt = `${eventInfo.title} ${params.title}`;
-    //     IconSvg = <img src={eventInfo.symbol.url} alt={alt}/>;
-    //   }
-    // }
-    IconSvg = IconSvg || ClockIcon;
+    let IconSvg = TAG_TO_SVG[params.iconTag] || ClockIcon;
     return value ? (
         <Col key={key} span={24} style={colStyle}>
           <IconSvg {...iconProps} title={params.title}/>
@@ -115,16 +112,6 @@ const EventsTooltip = observer(({store}) => {
         </Col>
     ) : null;
   })
-
-  // const timestamp = graphics.pop()['attributes']['insert_ts']
-  // const time = timestamp ? (
-  //   <Col key='insert_ts' span={24} style={colStyle}>
-  //     <ClockIcon {...iconProps} title='Detection time'/>
-  //     <span style={{fontSize: '14px'}}>
-  //       {new Date(timestamp).toLocaleString()}  
-  //     </span>
-  //   </Col>
-  // ) : null;
 
   return (
     <Card className="antd-esri-widget" style={style} size="small" title={title} extra={extra}>
