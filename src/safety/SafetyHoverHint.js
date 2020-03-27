f81fee64d8bb7fb61622adeb01ece9adfee3d289
dvorovyx <dmitryx.vorovyev@intel.com>
2020-03-27 13:27:19 +0300

    safety: display layers values in the hover hint

--- src/safety/SafetyHoverHint.js
+++ src/safety/SafetyHoverHint.js
@@ -17,9 +17,17 @@ export const SafetyHoverHint = observer(({store}) => {
   const aM = store.aliasMap;
   const quantile = getQuantile(attrs['risk_score']);
 
+  const content = store.filters
+    .filter(f => f.field === store.rendererField)
+    .map(f => (
+      <div className="gray" key={f.field}>
+        {f.alias}: {Math.round(attrs[f.field] * 1000) / 1000}
+      </div>
+  ));
+
   const point = store.hoverResults.screenPoint;
-  const left = point.x - 100;
-  const top = point.y - 60;
+  const left = point.x - 110;
+  const top = point.y - 65;
 
   return (
     <div className="details-hover" style={{left, top}}>
@@ -29,6 +37,7 @@ export const SafetyHoverHint = observer(({store}) => {
       <span className="details-hover__value uppercase" style={{color}}>
         {quantile.label}
       </span>
+      <span>{content}</span>
     </div>
   );
 });
