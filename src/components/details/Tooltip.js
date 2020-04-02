a6155aae38ebbfee27bbe59c569517b03963ecbc
dvorovyx <dmitryx.vorovyev@intel.com>
2020-04-02 15:41:42 +0300

    tooltip pointer

--- src/components/details/Tooltip.js
+++ src/components/details/Tooltip.js
@@ -1,9 +1,28 @@
 import './Tooltip.scss';
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { observer } from 'mobx-react';
 
 
 export const Tooltip = observer(({children, store, xMin=0, xMax=window.innerWidth}) => {
+  const ref = useRef(null);
+  const [width, setWidth] = useState(220);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (el && ResizeObserver) {
+      const observer = new ResizeObserver(payload => {
+        console.log(payload);
+        if (payload[0]) {
+          const dims = payload[0].contentRect;
+          const newWidth = Math.max(dims.width, 200) || 200;
+          setWidth(newWidth);
+        }
+      });
+      observer.observe(el);
+      return observer.disconnect;
+    }
+  }, [ref]);
+
   const results = store.hoverResults;
   let x, y;
   if (results) {
@@ -15,24 +34,38 @@ export const Tooltip = observer(({children, store, xMin=0, xMax=window.innerWidt
     y = -1000;
   }
 
-  const width = 220;
   const mid = width / 2;
   const pad = 10;
-  let left;
+  let left, pointerLeft;
   if (x > xMin + mid + pad) {
     if (x + mid + pad < xMax) {
       left = x - mid;
+      pointerLeft = x - left - 5
     } else {
-      left = xMax - pad - width;
+      left = xMax - pad * 3 - width;
+      pointerLeft = Math.min(width, x - left - 5);
     }
   } else {
     left = xMin + pad;
+    pointerLeft = Math.max(left - 5, x - left - 5);
   }
 
-  const top = y > 67 ? y - 65 : y + 10;
+  const height = 57;
+  let top, pointerTop;
+  if (y > height + pad) {
+    top = y - height - 15;
+    pointerTop = height - 5;
+  } else {
+    top = y + 30;
+    pointerTop = -5;
+  }
 
   return (
-    <div className="details-tooltip" style={{left, top}} >
+    <div className="details-tooltip" style={{left, top}} ref={ref} >
+      <div 
+        className="details-pointer" 
+        style={{left: pointerLeft, top: pointerTop}} 
+      />
       {children}
     </div>
   );
